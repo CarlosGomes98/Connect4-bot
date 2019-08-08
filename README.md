6f79085ada80fdae37cd4655ff989309a69ab0e6
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2019-08-08 20:02:53 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A Connect4-bot developed for the Vulcan Discord bot, that can also be used independently.
+
+Plays using Monte Carlo Tree search.
---
+++ game.js
@@ -0,0 +1,29 @@
+const Board = require("./board");
+const readline = require('readline-sync');
+const MCTS = require ("./mcts");
+
+let board = new Board(5, 5);
+
+let turn = 1;
+let win = false;
+let draw = false;
+do {
+    let move = null;
+    console.log(board.toString());
+    if (turn == 1)
+        move = parseInt(readline.question(`Player ${turn} move\n`));
+    else
+        move = MCTS(board, turn, 0);
+    try {
+        let state = board.makeMoveAndCheckWin(turn, move);
+        win = state["win"];
+        draw = state["draw"]
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+    if(!win) turn = (turn === 1 ? 2 : 1);
+} while (!(win || draw));
+
+console.log(board.toString());
+console.log(turn);
